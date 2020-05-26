a8d70b28a3e2e65ad03b7fa9c52f5608c7006b9c
NetMatt-2 <65219935+NetMatt-2@users.noreply.github.com>
2020-05-26 08:50:01 +0100

    +Testimonials; some basic edits to main - why the frick isn't npm compiling my changes and is it because I need to do a commit

--- resources/views/testimonials.blade.php
+++ resources/views/testimonials.blade.php
@@ -2,9 +2,23 @@
 
 @section('content')
 
+@include ('layouts.header')
+<body>
+<main>
 <h1>testimonials</h1>
+    <div class=testimonial>
+        <img src=# alt="video/mugshot here?">
+        <div class=tesimonial-text>
+            <h3>Joe Bloggs from Norfolk</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna</p>
+        </div>
+        <div class=quote>
+            <p>Lorem ipsum dolor sit amet</p>
+        </div>
+    </div>
 
-
-
+</main>
+</body>
+@include ('layouts.footer')
 
 @endsection
