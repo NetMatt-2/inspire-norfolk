85933950c9c2c04b9149614de7e26782a9babb3e
NetMatt-2 <65219935+NetMatt-2@users.noreply.github.com>
2020-06-01 07:37:56 +0100

    -

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -2,11 +2,11 @@
 
 @section('content')
 
-@include ('layouts.header')
+@include('layouts.header')
 
     <main>
         <h1>Our Services</h1>
-        <div class="Our Services">
+        <div class="our-services">
             <section>
                 <h2>Apprenticeship Recruitment</h2>
                 <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
@@ -22,6 +22,6 @@
         </div>
     </main>
 
-@include ('layouts.footer')
+@include('layouts.footer')
 
 @endsection
